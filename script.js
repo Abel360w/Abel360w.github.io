216ab222ecc326f563123421439d976877cc6576
Abel._ <152060504+Abel360w@users.noreply.github.com>
2024-02-22 20:57:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,5 +6,5 @@ function loadWebsite() {
     }
 
     const webview = document.getElementById('webview');
-    webview.innerHTML = `<iframe src="https://cors-anywhere.herokuapp.com/${url}" width="100%" height="400px"></iframe>`;
+    webview.innerHTML = `<iframe src="${url}" width="100%" height="400px"></iframe>`;
 }
